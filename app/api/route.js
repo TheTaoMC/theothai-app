137f52429f8fd4dacddede537179011158236ade
TheTaoMC <manamanote@gmail.com>
2024-03-01 16:30:12 +0700

    f5

--- app/api/route.js
+++ app/api/route.js
@@ -1,6 +1,22 @@
-export async function GET() {
+export const dynamic = 'force-dynamic' // defaults to auto
+export async function GET(request) {
     console.log('test');
     return Response.json({
-        title: 'POP ART'
+        title: 'POP ART 2024'
+    }, {
+        status: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
     })
+    /*     return new Response('POP ART 2024', {
+            status: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            },
+        }) */
 }
